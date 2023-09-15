059b0f694e7b0c35fe228e1b02fb16632096a901
sarah svitlik <svitliksarah@gmail.com>
2023-09-14 20:43:55 -0400

    working on getting the "getQuestion" to work
    Stil some bugs and syntax errors to fix

--- app/src/main/java/com/unh/svitlikicebreaker/MainActivity.kt
+++ app/src/main/java/com/unh/svitlikicebreaker/MainActivity.kt
@@ -2,17 +2,29 @@ package com.unh.svitlikicebreaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.unh.svitlikicebreaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+    private val TAG = "IcebreakerAndroidF23Tag"
 
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, msg:"Entered into OnCreate"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.ButtonGetQuestion.setOnClickListener {
+            Log.d(TAG, msg:"Button GetQuestion was pressed")
+        }
     }
+
+    override
+
+
 }
+

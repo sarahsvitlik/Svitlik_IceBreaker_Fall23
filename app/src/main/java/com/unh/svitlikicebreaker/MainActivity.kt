31c7e31359e225793bc0ab79204626a68f41786c
sarah svitlik <svitliksarah@gmail.com>
2023-09-22 11:14:43 -0400

    Added firebase to repository

--- app/src/main/java/com/unh/svitlikicebreaker/MainActivity.kt
+++ app/src/main/java/com/unh/svitlikicebreaker/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.unh.svitlikicebreaker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import com.unh.svitlikicebreaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater) //Allows us to grab our files in our view through this binding funnction
     private val TAG = "IcebreakerAndroidF23Tag"
+    private var db = Firebase.firestore
+    private var questionBank: MutableList<Questions> = arrayListOf()  //an empty list of type questions
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "Entered into OnCreate") //start all your code
         super.onCreate(savedInstanceState)
@@ -20,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding.ButtonGetQuestion.setOnClickListener {//container for actions when getQuestions is pressed
             Log.d(TAG, "Button GetQuestion was pressed")
             binding.txtAnswer.setText("Button was clicked")
-            getQuestionsfromFirebase()
+            `getQuestions-fromFirebase`()
         }
 
         //linked ui to code
@@ -42,9 +45,17 @@ class MainActivity : AppCompatActivity() {
         }
 
     //make private to protect the firebase questions
+    @SuppressLint("SetTextI18n")
     private fun getQuestionsfromFirebase() {
-        Log.d(TAG, "Fetching questions from databse")
-        binding.txtAnswer.setText("Doing Database stuff")
+        Log.d(TAG, "Fetching questions from database")
+
+        db.collection("questions")
+            .get()
+            .addOnSuccessListener {documents ->      //grab all of documents from database and store them
+            questionBank = mutableListOf()
+            for(document in documents)  {   //document your searching for in documents
+                Log.d(TAG "$(document.id"} =>     ${document.data"})
+            }//within the collection class, I have a function get...
     }
 
 }
